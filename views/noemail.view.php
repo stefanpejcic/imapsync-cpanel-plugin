82033fb86f9fec8889e08431bcf3d21981c25426
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2026-03-06 14:47:29 +0100

    Create noemail.view.php

---
+++ views/noemail.view.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" type="text/css" href="assets/css/main.css" media="screen" />
+<div class="container" style="width: auto">
+    <div class="content">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li><a style="cursor:pointer" onClick="window.location.assign(window.location.pathname);">Transfer</a></li>
+                        <li><a  target="_blank" href="#"> Pomoć</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <div>
+        <div class="col-12">
+            <div id="deleteError" class="alert alert-danger">
+                <span class="glyphicon glyphicon-remove-sign"></span>
+                <div class="alert-message">
+                    Trenutno nemate nijedan email nalog na koji bi ste mogli prebaciti mejlove. Napravite novi email nalog <a href="../email_accounts/index.html#/create">ovde</a>.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
